9950960aa97282b47e0f888d46684e744b953828
FARİZA <ahmetova.fariza@gmail.com>
2023-09-10 14:00:57 +0300

    dist folder

--- src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = "df57e421-1279-4bf6-8ccd-551ec13573fc";
-export const MODAL_CLOSE_SEC = 2.5;
